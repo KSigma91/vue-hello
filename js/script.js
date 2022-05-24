cb6be914be0cc451d48ad4049e3dd4226d07bcb3
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-24 16:56:13 +0200

    stamp

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+(va bene qualsiasi immagine che prendete dal web e/o avete e/o vi scaricate, è indifferente)
+*/
+
+// istanziamento
+var app = new Vue({
+    el: '#root',
+    data: {
+        message: "And after all we're only ordinary men"
+    }
+});
